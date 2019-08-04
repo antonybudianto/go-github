d9894b53e8f9359a9d1ae7e135d14db65aa93935
Antony Budianto <antonybudianto@gmail.com>
2019-08-04 10:33:12 +0700

    add grpc reflection api so grpcox works

--- cmd/grpc_server/server.go
+++ cmd/grpc_server/server.go
@@ -9,6 +9,7 @@ import (
 	pb "gogithub/protos"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 const (
@@ -28,6 +29,7 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
+	reflection.Register(s)
 	pb.RegisterHelloWorldServer(s, &helloworldserver{})
 	pb.RegisterGithubServiceServer(s, &github.GrpcServer{})
 	if err := s.Serve(lis); err != nil {
